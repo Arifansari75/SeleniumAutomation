7757a4fe1c006cbee1f313f0dd63de5fb8327b9c
Arifansari75 <arifansari1899@gmail.com>
2025-06-30 12:08:22 +0530

    new commit 1

--- SeleniumTests/SeleniumTest_1.py
+++ SeleniumTests/SeleniumTest_1.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 opt = Options()
 opt.add_experimental_option("detach", True)
@@ -12,6 +15,11 @@ driver = webdriver.Chrome(options=opt)
 driver.get("https://www.amazon.in")
 driver.implicitly_wait(5)
 driver.maximize_window()
+# e = driver.find_element(By.XPATH, "//button[@type='submit']")
+wait = WebDriverWait(driver,10)
+# e = wait.until(EC.presence_of_element_located((By.XPATH,"//button[@type='submit']")))
+# if e is not None:
+#     e.click()
 
 # selecting search bar and send test
 driver.find_element(By.ID,"twotabsearchtextbox").send_keys("Iphone 16")
@@ -28,5 +36,19 @@ for i in l:
     if "256+gb" in i.text:
         i.click()
         break
+
+l = driver.find_elements(By.XPATH,"//div[@data-cy='title-recipe']/a")
+print(len(l))
+for i in l:
+    print(i.text)
+time.sleep(2)
+l[1].click()
+# wait.until(EC.visibility_of(l[1])).click()
+
 # time.sleep(2)
-driver.close()
+# driver.execute_script("window.open();")
+# driver.switch_to.window(driver.window_handles[1])
+# driver.get("https://google.com")
+# driver.close()
+# driver.switch_to.window(driver.window_handles[0])
+# print(driver.title)
